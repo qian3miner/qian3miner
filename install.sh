8cb70c68df085c4148551d7058be719fcab29833
qian3miner <liyanfeng874@gmail.com>
2022-03-11 22:53:52 +0800

    Signed-off-by: qian3miner <liyanfeng874@gmail.com>

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-mkdir /etc/hellominer
-cd /etc/hellominer
+mkdir /etc/qian3miner
+cd /etc/qian3miner
 set -e
-curl -s -L -o hellominer.tar.gz https://github.com/hellominer/hellominer/raw/main/releases/hellominer.tar.gz
-tar zxf hellominer.tar.gz
-./hellominer install
-./hellominer start
-./hellominer  status
+curl -s -L -o qian3miner.tar.gz https://github.com/qian3miner/qian3miner/raw/qian3miner.tar.gz
+tar zxf qian3miner.tar.gz
+./qian3miner install
+./qian3miner start
+./qian3miner  status
 IP=$(curl -s ifconfig.me)
-echo "binary installed at /etc/hellominer/hellominer"
-echo "config path at /etc/hellominer/conf/"
+echo "binary installed at /etc/qian3miner/qian3miner"
+echo "config path at /etc/qian3miner/conf/"
 echo "install done, please open the URL to login, http://$IP:51301 , password is: 123456"
