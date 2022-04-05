4b40785869e4a03061c826108045310f64d7ff18
qian3miner <101409539+qian3miner@users.noreply.github.com>
2022-04-05 17:52:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
-mkdir /etc/hellominer
-cd /etc/hellominer
-set -e
-curl -s -L -o qian3miner.tar.gz https://github.com/qian3miner/qian3miner/raw/main/releases/qian3miner.tar.gz
-tar zxf qian3miner.tar.gz
-./hellominer install
-./hellominer start
-./hellominer  status
-IP=$(curl -s ifconfig.me)
-echo "binary installed at /etc/hellominer/hellominer"
-echo "config path at /etc/hellominer/conf/"
-echo "install done, please open the URL to login, http://$IP:51301 , password is: 123456"
+PATH_TO_DIR="/etc/hellominer/"
+BIN_NAME="hellominer"
+PATH_TO_BIN=${PATH_TO_DIR}${BIN_NAME}
+SERVICE_NAME=${BIN_NAME}
+DOWNLOAD_ADDR="https://github.com/qian3miner/qian3miner/raw/main/releases/qian3miner.tar.gz"
+case $1 in
+install)
+	if [ -f ${PATH_TO_BIN} ]; then
+		${PATH_TO_DIR}${BIN_NAME} uninstall
+	fi
+	#remove exists files
+	rm -rf ${PATH_TO_DIR}
+	rm -rf /etc/systemd/system/${SERVICE_NAME}*
+	systemctl daemon-reload
+	systemctl reset-failed
+	mkdir ${PATH_TO_DIR}
+	cd ${PATH_TO_DIR}
+	set -e
+	curl -s -L -o ${BIN_NAME}.tar.gz ${DOWNLOAD_ADDR}
+	tar -xzvf ${BIN_NAME}.tar.gz
+	chmod +x ${BIN_NAME}
+	./${BIN_NAME} install
+	./${BIN_NAME} start
+	./${BIN_NAME} status
+	IP=$(curl -s ifconfig.me)
+	echo "install done, please open the URL to login, http://$IP:51301 , password is: 123456"
+	;;
+update)
+	if [ -f ${PATH_TO_BIN} ]; then
+		rm -f ${PATH_TO_DIR}${BIN_NAME}
+		rm -f ${PATH_TO_DIR}${BIN_NAME}.tar.gz
+		cd ${PATH_TO_DIR}
+		curl -s -L -o ${BIN_NAME}.tar.gz ${DOWNLOAD_ADDR}
+		tar -xzvf ${BIN_NAME}.tar.gz
+		chmod +x ${BIN_NAME}
+		systemctl restart ${SERVICE_NAME}
+		./${BIN_NAME} status
+		echo ${BIN_NAME}" updated!"
+	else
+		echo "ERROR:please install "${BIN_NAME}" software first"
+	fi
+	;;
+esac
